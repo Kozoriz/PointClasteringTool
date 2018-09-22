47f8b48120cb35e02e41f08ae01d379924ebb765
Kozoriz <kozorizandriy@gmail.com>
2018-09-22 16:23:18 +0300

    Add logger, fix buildability

--- src/Model/PointCloudManager/PointCloudManager.cpp
+++ src/Model/PointCloudManager/PointCloudManager.cpp
@@ -1,10 +1,18 @@
 #include "PointCloudManager.h"
 
 #include "utils/file_system.h"
+#include "utils/logger.h"
+
+CREATE_LOGGER("PointCloudManager")
 
 void PointCloudManager::LoadNewCloud(utils::String &sPath)
 {
+  utils::file_system::File PCFile(sPath);
+  PCFile.Open(utils::file_system::File::OpenMode::Read);
+  if(!PCFile.IsOpened())
+  {
 
+  }
 }
 
 void PointCloudManager::RunClasteringProcess(PointCloudManager::PointCloud &cloud)

d29df596317a109d841a15f58e320a949883cd2f
Vova <wzabila1995@gmail.com>
2018-11-10 12:01:25 +0200

    Rework ui.

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -1,4 +1,4 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 
 #include "utils/logger.h"
@@ -10,7 +10,7 @@
 int
 main( int argc, char* argv[] )
 {
-    QGuiApplication app( argc, argv );
+    QApplication app( argc, argv );
 
     logger::Logger::InitLogger("log4cplus.properties"); // use app settings
     LifeCycle lifeCycle;
